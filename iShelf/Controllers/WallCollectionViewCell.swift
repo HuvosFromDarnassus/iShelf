34feb21b51cd28e4a2053d73cc5103643b3af585
Daniel Tvorun <danieltvorun@icloud.com>
2022-04-19 11:20:06 +0300

    backgrounds collection view class structure

---
+++ iShelf/Controllers/WallCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  WallCollectionViewCell.swift
+//  iShelf
+//
+//  Created by Daniel Tvorun on 19.04.2022.
+//
+
+import UIKit
+
+class WallCollectionViewCell: UICollectionViewCell {
+    
+}
